6313880c2797e88feb515bdfc51d663a17909c2f
Lucas Vivier <79191733+lucas-vivier@users.noreply.github.com>
2022-08-04 09:47:18 +0200

    multiprocessing

--- project/main.py
+++ project/main.py
@@ -15,7 +15,6 @@
 #
 # Original author Lucas Vivier <vivier@centre-cired.fr>
 
-import pandas as pd
 import os
 import copy
 import logging
@@ -25,121 +24,11 @@ from multiprocessing import get_context, Pool
 from datetime import datetime
 import re
 import argparse
-import traceback
 
-from building import AgentBuildings
-from input.param import generic_input
-from read_input import read_stock, read_policies, read_exogenous, read_revealed, parse_parameters
-from write_output import parse_output, grouped_output
+from write_output import grouped_output
+from model import res_irf
 
 LOG_FORMATTER = '%(asctime)s - %(process)s - %(name)s - %(levelname)s - %(message)s'
-
-
-def res_irf(config, path):
-    """Res-IRF model.
-
-    Parameters
-    ----------
-    config: dict
-        Scenario-specific input
-    path: str
-        Scenario-specific output path
-
-    Returns
-    -------
-    str
-        Scenario name
-    pd.DataFrame
-        Detailed results
-    """
-    os.mkdir(path)
-
-    # logging.getLogger('matplotlib.font_manager').disabled = True
-    # logging.getLogger('matplotlib.axes').disabled = True
-
-    """logger = logging.getLogger('log_{}'.format(path.split('/')[-1].lower()))
-    logger.setLevel('DEBUG')
-    logger.propagate = False
-    # consoler handler
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(logging.Formatter(LOG_FORMATTER))
-    logger.addHandler(console_handler)
-    # file handler
-    file_handler = logging.FileHandler(os.path.join(path, 'log.log'))
-    file_handler.setFormatter(logging.Formatter(LOG_FORMATTER))
-    logger.addHandler(file_handler)"""
-
-    try:
-        # logger.debug('Reading input')
-
-        stock, year = read_stock(config)
-        policies_heater, policies_insulation, taxes = read_policies(config)
-        param, summary_param = parse_parameters(config, generic_input, stock)
-        energy_prices, energy_taxes, cost_heater, cost_insulation = read_exogenous(config)
-        efficiency, choice_insulation, ms_heater, restrict_heater, choice_heater, renovation_rate_ini, ms_intensive = read_revealed(config)
-
-        if config['prices_constant']:
-            energy_prices = pd.concat([energy_prices.loc[year, :]] * energy_prices.shape[0], keys=energy_prices.index,
-                                      axis=1).T
-
-        total_taxes = pd.DataFrame(0, index=energy_prices.index, columns=energy_prices.columns)
-        for t in taxes:
-            total_taxes = total_taxes.add(t.value, fill_value=0)
-
-        if energy_taxes is not None:
-            total_taxes = total_taxes.add(energy_taxes, fill_value=0)
-
-        if config['taxes_constant']:
-            total_taxes = pd.concat([total_taxes.loc[year, :]] * total_taxes.shape[0], keys=total_taxes.index,
-                                    axis=1).T
-
-        energy_vta = energy_prices * generic_input['vta_energy_prices']
-        total_taxes += energy_vta
-
-        energy_prices = energy_prices.add(total_taxes, fill_value=0)
-        param['energy_prices'] = energy_prices
-
-        t = total_taxes.copy()
-        t.columns = t.columns.map(lambda x:  'Taxes {} (euro/kWh)'.format(x))
-        temp = energy_prices.copy()
-        temp.columns = temp.columns.map(lambda x:  'Prices {} (euro/kWh)'.format(x))
-        pd.concat((summary_param, t, temp), axis=1).to_csv(os.path.join(path, 'input.csv'))
-
-        # logger.debug('Creating AgentBuildings object')
-        buildings = AgentBuildings(stock, param['surface'], generic_input['ratio_surface'], efficiency, param['income'],
-                                   param['consumption_ini'], path, param['preferences'],
-                                   restrict_heater, ms_heater, choice_insulation, param['performance_insulation'],
-                                   year=year, demolition_rate=param['demolition_rate'],
-                                   data_calibration=param['data_ceren'], endogenous=config['endogenous'],
-                                   number_exogenous=config['exogenous_detailed']['number'], logger=None)
-
-        # logger.debug('Calibration energy consumption {}'.format(year))
-        buildings.calculate(energy_prices.loc[year, :], taxes)
-        for year in range(config['start'] + 1, config['end']):
-            # logger.debug('Run {}'.format(year))
-            buildings.year = year
-            buildings.add_flows([- buildings.flow_demolition()])
-            flow_retrofit = buildings.flow_retrofit(energy_prices.loc[year, :], cost_heater, ms_heater, cost_insulation,
-                                                    ms_intensive, renovation_rate_ini,
-                                                    [p for p in policies_heater if (year >= p.start) and (year < p.end)],
-                                                    [p for p in policies_insulation if (year >= p.start) and (year < p.end)],
-                                                    config['target_freeriders'],
-                                                    supply_constraint=config['supply_constraint'])
-            buildings.add_flows([flow_retrofit, param['flow_built'].loc[:, year]])
-            buildings.calculate(energy_prices.loc[year, :], taxes)
-
-        # logger.debug('Writing output')
-        stock, output = parse_output(buildings, param)
-        output.round(3).to_csv(os.path.join(path, 'output.csv'))
-        stock.round(2).to_csv(os.path.join(path, 'stock.csv'))
-
-        return os.path.basename(os.path.normpath(path)), output, stock
-
-    except Exception as e:
-        # logger.exception(e)
-        # logging.error(traceback.format_exc())
-        raise e
-
 
 if __name__ == '__main__':
     start = time()
