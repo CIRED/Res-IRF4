42c27f8f967445964b52f56e6145f1f52efcd29a
Lucas Vivier <79191733+lucas-vivier@users.noreply.github.com>
2023-09-12 11:46:58 +0200

    removing revealed_data

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='ResIRF',
     version='4.0',
-    packages=['project', 'project.input', 'project.input.revealed_data', 'project.input.policies',
+    packages=['project', 'project.input', 'project.input.policies',
               'project.input.resources_dir'],
     url='',
     license='',
     author='Lucas Vivier',
-    author_email='vivier@centre-cired.fr',
+    author_email='lucas.vivier@enpc.fr',
     description='Building stock model'
 )
